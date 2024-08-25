6af3dd95847a56afeb21c077be3afc2b28218357
Ashutosh Pandey <akpandey1928@gmail.com>
2024-08-25 17:01:07 +0530

    add: frontend v1

--- frontend/src/components/DbPostStats.tsx
+++
@@ -1,8 +0,0 @@
-export const DbPostStats = () => {
-	return (
-		<div className="sticky top-4 flex-[0.2] w-full h-[100px] mr-8 p-5 border-2 border-slate-400">
-			<p className="font-semibold text-lg">Posts in Queue : {"100"}</p>
-			<p className="font-semibold text-lg">Posts in DB : {"200"}</p>
-		</div>
-	);
-};
